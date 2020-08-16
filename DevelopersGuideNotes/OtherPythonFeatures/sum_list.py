9fe1a5b36343b15627c06e419de27f328f65138c
minefarmer <pgoldfarm@yahoo.com>
2020-08-16 11:37:45 -0500

    cleaning up

--- DevelopersGuideNotes/OtherPythonFeatures/sum_list.py
+++ DevelopersGuideNotes/OtherPythonFeatures/sum_list.py
@@ -1,1 +1,11 @@
 numbers_file = open('numbers.list', 'r')
+number_list = numbers_file.read().splitlines()
+
+sum = 0
+
+for number in number_list:
+    sum = int(number)
+
+print("The sum of all your numbers was: ")
+print(sum)
+
