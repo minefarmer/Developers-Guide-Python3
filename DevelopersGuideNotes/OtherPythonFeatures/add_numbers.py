d3978b5c5492ed619b91427d4e0cd63dbb9edf11
minefarmer <pgoldfarm@yahoo.com>
2020-08-16 21:19:23 -0500

    nighty nite.

---
+++ DevelopersGuideNotes/OtherPythonFeatures/add_numbers.py
@@ -0,0 +1,43 @@
+def add(num1, num2):
+    return int(num1) + int(num2)
+
+def subtract(num1, num2):
+    return int(num1) - int(num2)
+
+def multiply(num1, num2):
+    return int(num1) * int(num2)
+
+def divide(num1, num2):
+    return int(num1) / int(num2)
+
+num_1 = input("Please enter a number: ")
+
+num_2 = input("Please enter a second number: ")
+
+operator = input("Select an operation to perform: [add, subtract, multiply, divide]")
+results_list = open('result_list', 'r').read().splitlines()
+if operator =='add':
+    result = add(num_1, num_2)
+    
+elif operator =='subtract':
+    result = subtract(num_1, num_2)
+    
+elif operator =='multiply':
+    result = multiply(num_1, num_2)
+    
+elif operator =='divide':
+    result = divide(num_1, num_2)
+    
+else:
+    result = 0
+    print("Invalid operator")
+    
+results_list.append(result)
+print(results_list)
+result_file = open('result_list', 'w')
+for result in results_list:
+    result_file.write(str(result)+"\n")
+result_file.close()
+    
+print("Your result was "+ str(result))
+
--- DevelopersGuideNotes/OtherPythonFeatures/sum_list.py
+++ DevelopersGuideNotes/OtherPythonFeatures/sum_list.py
@@ -4,8 +4,8 @@ number_list = numbers_file.read().splitlines()
 sum = 0
 
 for number in number_list:
-    sum = int(number)
+    sum = sum+int(number)
 
-print("The sum of all your numbers was: ")
-print(sum)
+print("The sum of all your numbers was: ")  # The sum of all your numbers was: 
+print(sum)  # 400   ***  after 200 and 225 were added to the
 
